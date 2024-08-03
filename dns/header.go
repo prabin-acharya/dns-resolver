b63d6617d35296d9e6f28c253b9d729dfec11a1d
Prabin Acharya <acharyaprabin101@gmail.com>
2024-08-04 00:03:43 +0545

    header structure

---
+++ dns/header.go
@@ -0,0 +1,23 @@
+// https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
+
+package dns
+
+type Header struct {
+	ID uint16 // 16 bit identification number
+
+	// flags - 16 bit
+	QR     bool  // query/response flag
+	Opcode uint8 // purpose of message
+	AA     bool  // authoritative answer
+	TC     bool  // truncated message
+	RD     bool  // recursion desired
+	RA     bool  // recursion available
+	Z      uint8 // reserved
+	Rcode  uint8 // response code
+
+	// 16 bit each
+	QDCount uint16 // number of question entries
+	ANCount uint16 // number of answer entries
+	NSCount uint16 // number of authority entries
+	ARCount uint16 // number of resource entries
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prabin-acharya/dns-resolver
+
+go 1.22.3
